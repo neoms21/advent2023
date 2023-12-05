3a50d33ead4a89714b3ce0cb1d72c4f282f21c0d
Manoj Sethi <mjsh@gft.com>
2023-12-05 09:24:45 +0000

    day 4 part 2

--- day4/solution.py
+++ day4/solution.py
@@ -10,26 +10,53 @@ def score_from_range(total:int):
             score*=2
     return score
 
-
-def solution(input):
-    sum=0
-    for item in input:
-        numbers = item.split(':')[1]
-        winning_numbers = [e for e in numbers.split('|')[0].split(' ') if e!='']
-        all_numbers = [e for e in numbers.split('|')[1].split(' ') if e!='']
-        winning_dict={}
-        all_dict={}
-        for n in winning_numbers:
-            winning_dict[n]=True
-        for n in all_numbers:
-            all_dict[n]=True
-        total_points=0
-        j=1
-        for k in winning_dict:
-            if(all_dict.get(k)!=None):
-                    total_points+=1
-                
+def get_points(item:str):
+    numbers = item.split(':')[1]
+    winning_numbers = [e for e in numbers.split('|')[0].split(' ') if e!='']
+    all_numbers = [e for e in numbers.split('|')[1].split(' ') if e!='']
+    winning_dict={}
+    all_dict={}
+    for n in winning_numbers:
+        winning_dict[n]=True
+    for n in all_numbers:
+        all_dict[n]=True
+    total_points=0
+    j=1
+    for k in winning_dict:
+        if(all_dict.get(k)!=None):
+            total_points+=1
         
-        sum+=score_from_range(total_points)
+    return total_points    
+
+cards = []
+
+    
+
+part2Total = 0 
+def recursion(next_cards_count,index, input):
+        global part2Total       
+        if(next_cards_count>0):
+            i = index+1
+            while i < next_cards_count+index+1:
+                next_cards = get_points(input[i])
+                part2Total+=next_cards
+                if(next_cards):
+                    recursion(next_cards,i,input)
+                i += 1
+                
+   
+def part2(input):
+    global part2Total
+    for index,item in enumerate(input):
+        print(f"==>> item: {item}")
+        
+        part2Total+=1
+        next_cards_count = get_points(item)
+        part2Total+=next_cards_count
+        recursion(next_cards_count, index, input)
+        
+           
+        
             
-    print(sum)
+    
+    print(f"==>> part2Total: {part2Total}")
