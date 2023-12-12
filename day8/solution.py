09e01c3e0366244e9952130f6a929162bbe2953e
Manoj Sethi <mjsh@gft.com>
2023-12-12 23:09:59 +0000

    day 8 part 2 - unsolved

--- day8/solution.py
+++ day8/solution.py
@@ -2,30 +2,13 @@ direction_map={
     'L':0,
     'R':1
 }
-directions=''
-result_found = False
-first_key = 'AAA'
-def solution(input):
-
+          
+def part1(nodes, directions):
     resultant_key ='ZZZ'
-    nodes = {}
-    for i, item in enumerate(input):
-        if(i==0):
-            directions=item
-        elif(i==1):
-            continue
-        else:
-            
-            s1 = item.split(' = ')
-            key = s1[0]
-            val= s1[1]
-            
-            y=val.replace("(","").replace(')',"").split(', ')
-            nodes[key] = (y[0],y[1])
-            
-    # print(nodes)
-    global result_found
-    global first_key
+   
+    result_found = False
+    first_key = 'DTA'
+    
     runs = 0
     while(result_found!=True):
         for d in directions:
@@ -33,13 +16,58 @@ def solution(input):
             tuple_index = direction_map[d]
             # print(f"Before==>> node_key: {first_key} - {d}")
             first_key = nodes[first_key][tuple_index]
-            print(f"After==>> node_key: {first_key} - {d}")
+            # print(f"After==>> node_key: {first_key} - {d}")
             if(first_key== resultant_key):
                 break
            
-        print(runs,{first_key})
-        if(first_key==resultant_key):
+        if(first_key[2]=='Z'):
             result_found=True
     
-    print(f'Total iterations={runs}')
+    print(f'Total iterations = {runs}')
+
+
+def is_final_nodes(nodes):
+    filtered_nodes = [n for n in list(nodes) if n[2]=='Z']
+    return len(filtered_nodes)==len(nodes)
+
+def part2(nodes, directions):
+    result_found = False
+    runs = 0
+    starting_nodes = [n for n in list(nodes.keys()) if n[2]=='A']
+    print(f"==>> starting_nodes: {starting_nodes}")
+    
+    next_nodes = []
+    while(result_found!=True):
+        for d in directions:
+            for n in starting_nodes:
+                tuple_index = direction_map[d]
+                next_nodes.append(nodes[n][tuple_index])
+            starting_nodes = next_nodes
+            print(f"==>> starting_nodes: {starting_nodes}")
+            next_nodes=[]
+            runs+=1
+            # print(runs)
+       
+        if(runs>100 or is_final_nodes(starting_nodes)==True):
+            result_found= True
             
+    print(runs)
+def solution(input):
+     nodes = {}
+     directions=''
+     for i, item in enumerate(input):
+        if(i==0):
+            directions=item
+        elif(i==1):
+            continue
+        else:    
+            s1 = item.split(' = ')
+            key = s1[0]
+            val= s1[1]
+            
+            y=val.replace("(","").replace(')',"").split(', ')
+            nodes[key] = (y[0],y[1])
+    
+     part1(nodes, directions)
+    #  part2(nodes, directions)
+  
