d62cf3bae2436688add3abd86aef03212529cafb
Manoj Sethi <mjsh@gft.com>
2023-12-13 09:10:17 +0000

    day 8 part 2

--- day8/solution.py
+++ day8/solution.py
@@ -1,57 +1,71 @@
+
+from itertools import cycle
+import re
+import math
+
 direction_map={
     'L':0,
     'R':1
 }
+with open('day8/input.txt', 'r') as f:
+    puzzle_input = f.read()
           
-def part1(nodes, directions):
-    resultant_key ='ZZZ'
+def part1(nodes, directions, first_key='AAA'):
    
     result_found = False
-    first_key = 'DTA'
     
     runs = 0
     while(result_found!=True):
         for d in directions:
             runs+=1
             tuple_index = direction_map[d]
-            # print(f"Before==>> node_key: {first_key} - {d}")
             first_key = nodes[first_key][tuple_index]
-            # print(f"After==>> node_key: {first_key} - {d}")
-            if(first_key== resultant_key):
+            if(first_key[2]=='Z'):
                 break
            
         if(first_key[2]=='Z'):
             result_found=True
     
     print(f'Total iterations = {runs}')
+    return runs
 
 
 def is_final_nodes(nodes):
     filtered_nodes = [n for n in list(nodes) if n[2]=='Z']
-    return len(filtered_nodes)==len(nodes)
+    return len(filtered_nodes)==2
 
 def part2(nodes, directions):
-    result_found = False
-    runs = 0
+    runs = []
     starting_nodes = [n for n in list(nodes.keys()) if n[2]=='A']
-    print(f"==>> starting_nodes: {starting_nodes}")
     
-    next_nodes = []
-    while(result_found!=True):
-        for d in directions:
-            for n in starting_nodes:
-                tuple_index = direction_map[d]
-                next_nodes.append(nodes[n][tuple_index])
-            starting_nodes = next_nodes
-            print(f"==>> starting_nodes: {starting_nodes}")
-            next_nodes=[]
-            runs+=1
-            # print(runs)
+    for n in starting_nodes:
+        r = part1(nodes,directions,n)
+        runs.append(r)
        
-        if(runs>100 or is_final_nodes(starting_nodes)==True):
-            result_found= True
-            
-    print(runs)
+       
+    print(math.lcm(*runs))
+    
+    
+def part2_gh(puzzle_input):
+    directions, connections = puzzle_input.split('\n\n')
+    directions = [0 if d == 'L' else 1 for d in directions]
+    graph = {}
+    regex = r'(\w{3}) = \((\w{3}), (\w{3})\)'
+    for node, left, right in re.findall(regex, connections):
+        graph[node] = [left, right]
+
+    starting_nodes = [node for node in graph if node[2] == 'A']
+    cycles = []
+    for node in starting_nodes:
+        for steps, d in enumerate(cycle(directions), start=1):
+            node = graph[node][d]
+            if node[2] == 'Z':
+                cycles.append(steps)
+                break
+
+    print(f"==>> cycles: {cycles}")
+    return math.lcm(*cycles)
+
 def solution(input):
      nodes = {}
      directions=''
@@ -69,5 +83,6 @@ def solution(input):
             nodes[key] = (y[0],y[1])
     
      part1(nodes, directions)
-    #  part2(nodes, directions)
+     part2(nodes, directions)
+    #  print(part2_gh(puzzle_input))
   
