ee591eabf5f40c143f034d08e41d30aee65fba56
Manoj Sethi <neoms21@gmail.com>
2025-12-03 19:57:29 +0000

    day 2 part 2

--- day2/solution.py
+++ day2/solution.py
@@ -3,16 +3,7 @@ import re
 # def part2:
 
 
-# def part1(password):
-#     return password
-
-
-# def part2(password, additional):
-#     return password + additional
-
-
-def solution(input: str):
-    ranges = input[0].split(",")
+def part1(ranges):
     total = 0
     for rng in ranges:
         lower, upper = rng.split("-")
@@ -23,4 +14,34 @@ def solution(input: str):
             if first_half == second_half:
                 total = total + x
 
-    print(total)
+    return total
+
+
+def part2(ranges):
+    total = 0
+    for rng in ranges:
+        lower, upper = rng.split("-")
+        for line in range(int(lower), int(upper) + 1, 1):
+
+            n = len(str(line)) // 2
+            # print(f"==>> n: {line}")
+
+            for l in range(n):
+                # print(f".   ==>> l: {l}")
+                split = [
+                    str(line)[i : (i + l + 1)]
+                    for i in range(0, len(str(line)), (l + 1))
+                ]
+                is_match = all(x == split[0] for x in split)
+                if is_match:
+                    total = total + line
+                    break
+
+    return total
+
+
+def solution(input: str):
+    ranges = input[0].split(",")
+
+    # print(part1(ranges))
+    print("Total", part2(ranges))
